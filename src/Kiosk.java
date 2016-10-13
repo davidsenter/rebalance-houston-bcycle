c529e6a3872ebfdc67ccef88245810a25ce99f41
davidsenter <davidsenter@Davids-MacBook-Air.local>
2016-10-12 22:23:00 -0500

    Initial commit

---
+++ src/Kiosk.java
@@ -0,0 +1,11 @@
+
+public class Kiosk {
+	private double latitude;
+
+	public Kiosk(double lat) {
+		latitude = lat;
+	}
+	public double getLat(){
+		return latitude;
+	}
+}
---
+++ src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Houston");
+		Kiosk kiosk1 = new Kiosk(84.0001);
+	}
+
+}
