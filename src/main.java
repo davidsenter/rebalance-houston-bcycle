75bbba620361b5e8756a71554bf43d84bd0e1a97
LataneBullock <1prlabu1@gmail.com>
2016-10-13 10:52:27 -0500

    Changes 13 Oct
    Prelim Tutorial

--- src/main.java
+++ src/main.java
@@ -5,7 +5,22 @@ public class main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Houston");
 		System.out.println("Fix bikes");
+		System.out.println("Lat was here");
 		Kiosk kiosk1 = new Kiosk(84.0001);
+		Kiosk kiosk2 = new Kiosk(84.2001);
+		
+		int x = 5;
+		int y = 4;
+		int z = x +y;
+		
+		double temp = kiosk1.getLat();
+		
+		phase1(kiosk1);
+		
+	}
+	
+	private static void phase1(Kiosk kiosk){
+		// phase 1 code
 	}
 
 }
