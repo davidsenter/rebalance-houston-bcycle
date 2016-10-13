5568929ca712f7170bc4361f84ad3f756ac4e891
LataneBullock <1prlabu1@gmail.com>
2016-10-13 11:13:24 -0500

    Test Changes
    No significant updates: simple changes to get familiar with the code

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,11 +1,29 @@
 
 public class Kiosk {
 	private double latitude;
+	private double longitude;
+	private String kioskID;
+	private String referenceName;
 
-	public Kiosk(double lat) {
+	public Kiosk(double lat, double lon, String id, String refName) {
 		latitude = lat;
+		longitude = lon;
+		kioskID = id;
+		referenceName = refName;
 	}
+	
 	public double getLat(){
 		return latitude;
 	}
+	public double getLon(){
+		return longitude;
+	}
+	public String getReferenceName() {
+		return referenceName;
+	}
+	public String getkioskID() {
+		return kioskID;
+	}
+	
+	
 }
--- src/main.java
+++ src/main.java
@@ -6,8 +6,9 @@ public class main {
 		System.out.println("Hello Houston");
 		System.out.println("Fix bikes");
 		System.out.println("Lat was here");
-		Kiosk kiosk1 = new Kiosk(84.0001);
-		Kiosk kiosk2 = new Kiosk(84.2001);
+		Kiosk kiosk1 = new Kiosk(84.0001, 82.00, "houston-bcycle-3211","Main and 4th");
+		Kiosk kiosk2 = new Kiosk(83.2, -81.52, "houston-bcycle-3213","Main and 6th");
+		//Kiosk kiosk2 = new Kiosk(84.2001);
 		
 		int x = 5;
 		int y = 4;
@@ -16,6 +17,7 @@ public class main {
 		double temp = kiosk1.getLat();
 		
 		phase1(kiosk1);
+		System.out.println("kiosk2 reference name: " + kiosk2.getReferenceName());
 		
 	}
 	
